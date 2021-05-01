5a937c266ba2cbd8a8cb5bbe4c530983c35abf5e
Josue Cruz Santiago <joshuak500@hotmail.com>
2021-05-01 00:25:46 -0500

    Update paypal.php

--- amalurjewerly.com.mx/carrito/paypal/paypal.php
+++ amalurjewerly.com.mx/carrito/paypal/paypal.php
@@ -1,9 +1,8 @@
 <?php
 define('ProPayPal', 0);
 if(ProPayPal){
-    define("PayPalClientId", "ASUIXToQxRGY2dYC-LkcGAvMJtRjb_mQ3RlqK7xhh2uMdT0xaRUgzHgmQNh0_tA-DdWCT1KeneFvPoI8");
-    define("PayPalSecret", "EK4xs6VpSNhwIVZUAlIpdbtNx_9hqPoDqhWTvPeTS0y2M0eYWTzNwEzm9H5xmXYvMZaZJMJomS9qALWj");
-    define("PayPalBaseUrl", "https://api.paypal.com/v1/");
+define("PayPalClientId", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+    define("PayPalSecret", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");    define("PayPalBaseUrl", "https://api.paypal.com/v1/");
     define("PayPalENV", "production");
 } else {
     define("PayPalClientId", "AYk5PQAmb0RQyPZ6CvjUrTJHKBBMiUmX2gRFm_RwUFtsoKYbICw22qQ2HuV4Vy2TuCmxIewCKSTnqcrh");
